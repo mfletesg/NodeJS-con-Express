6b982b8ac00878f846ff60b1420a6b81fccbce37
Miguel Fletes <mfletesg@gmail.com>
2022-05-30 17:29:47 -0500

    deploy

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
 const whitelist = ['http://localhost:8080', 'http://myapp.co']
 const options = {
   origin : (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     }
     else{
